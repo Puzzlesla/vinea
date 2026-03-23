d479f141188eb806ca1e2149c0a97044f8476b81
Fatima Bala <fatimabala@icloud.com>
2026-03-23 02:25:55 -0400

    created .env file for security measures

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET } from '../.env'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCf6hv8s0RYyT9a4nF1kLseAIl0e1pRfnk",
-  authDomain: "ada2-87122.firebaseapp.com",
-  projectId: "ada2-87122",
-  storageBucket: "ada2-87122.firebasestorage.app",
-  messagingSenderId: "107353079111",
-  appId: "1:107353079111:web:bb69d8a272ac7bd3ea6062",
-  measurementId: "G-F4MJF7R5T3"
-};
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: '107353079111',
+  appId: '1:107353079111:web:bb69d8a272ac7bd3ea6062',
+  measurementId: 'G-F4MJF7R5T3',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+
+export { auth, googleProvider }
