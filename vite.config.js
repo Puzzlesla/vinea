2ff6ae175ef345e6ab560efaf2a5e4c66db9369a
Fatima Bala <fatimabala@icloud.com>
2026-03-24 12:22:02 -0400

    added depencies and updated vite to recognize js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'treat-js-as-jsx',
+      async transform(code, id) {
+        if (!/\/src\/.*\.js(\?.*)?$/.test(id)) return null
+
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },
+    react(),
+  ],
+  esbuild: {
+    include: /src\/.*\.js$/,
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
